4f568d9f11442d26f94661ae990b9f20f03a047d
Austin Theriot <austinmtheriot@gmail.com>
2023-08-03 11:18:17 -0500

    add essential nvim plugins

--- .config/nvim/lua/austin/packer.lua
+++ .config/nvim/lua/austin/packer.lua
@@ -7,9 +7,27 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- enable telescope plugin
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  -- enable rose color theme
+  use({ 'rose-pine/neovim', as = 'rose-pine', config = function() 
+	vim.cmd('colorscheme rose-pine')
+  end })
+
+  -- enable syntax highlighting - see https://github.com/nvim-treesitter/nvim-treesitter
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+  -- enable harpoon
+  use('theprimeagen/harpoon')
+
+  -- for branching undos
+  use('mbbill/undotree')
+
+  -- for integrating git with vim
+  use('tpope/vim-fugitive')
 end)

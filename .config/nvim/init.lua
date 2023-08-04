3d2e8c12ce9c0f3b2505c2630801c28b89d6e10f
Austin Theriot <austinmtheriot@gmail.com>
2023-08-04 07:38:08 -0500

    switch theme to VS Code theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -129,13 +129,40 @@ require('lazy').setup({
     },
   },
 
+  -- gemme sommma that sweet VS Code nostalgia
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'Mofiqul/vscode.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
+      vim.o.background = 'dark'
+      local c = require('vscode.colors').get_colors()
+      require('vscode').setup({
+        -- Alternatively set style in setup
+        -- style = 'light'
+
+        -- Enable transparent background
+        transparent = true,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Override colors (see ./lua/vscode/colors.lua)
+        color_overrides = {
+          vscLineNumber = '#FFFFFF',
+        },
+
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+          -- this supports the same val table as vim.api.nvim_set_hl
+          -- use colors from this colorscheme by requiring vscode.colors!
+          Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+        }
+      })
+      require('vscode').load()
+    end
   },
 
   {

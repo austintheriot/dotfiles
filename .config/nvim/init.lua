7398d0c61d71c04ab01b833627fa1554faf838f5
Austin Theriot <austinmtheriot@gmail.com>
2023-08-05 15:27:15 -0500

    set relative line numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -246,6 +246,9 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
+-- Eneable relative line numbers
+vim.wo.relativenumber = true;
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 

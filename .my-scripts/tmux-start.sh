cce5daa71b625f3101866d216becb00eaefe0a35
Austin Theriot <austin@gingerlabs.com>
2023-09-18 11:12:24 -0500

    startup script opens existing session if it exists

--- .my-scripts/tmux-start.sh
+++ .my-scripts/tmux-start.sh
@@ -1,5 +1,15 @@
-tmux new-session -d -s code
-tmux split-window -h
-tmux split-window -v 
-tmux select-pane -L
-tmux attach -t code
+# check for session already open
+SESSION_NAME='code'
+if [[ "$(tmux ls | rg $SESSION_NAME)" = "" ]]; then
+  # open new session
+  tmux new-session -d -s $SESSION_NAME
+  tmux split-window -h
+  tmux split-window -v 
+  tmux select-pane -L
+  tmux attach -t $SESSION_NAME
+else tmux new-session -d -s $SESSION_NAME
+  # open existing session
+  tmux attach -t $SESSION_NAME
+fi
+
+

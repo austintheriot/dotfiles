cf62b06ae33e2deff8aea067a5ac86d9d18dfa6e
Austin Theriot <austinmtheriot@gmail.com>
2023-09-17 21:58:28 -0500

    clean up init.lua file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,40 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-  And then you can explore or search through `:help lua-guide`
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -285,18 +248,11 @@ require('lazy').setup({
   -- Enable treesitter to auto-close and auto-update HTML/JSX tags
   'windwp/nvim-ts-autotag',
 
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
+  -- default kickstart plugins
   require 'kickstart.plugins.autoformat',
   require 'kickstart.plugins.debug',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
+  -- optional custom plugins (not used yet)
   -- { import = 'custom.plugins' },
 }, {})
 
@@ -549,11 +505,11 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- PYTHON VIRTUALENV SETUP
-vim.g.python3_host_prog = '/Users/austintheriot/.pyenv/versions/nvim-general/bin/python'
+-- PYTHON VIRTUALENV SETUP (not used currently)
+-- vim.g.python3_host_prog = '/Users/austintheriot/.pyenv/versions/nvim-general/bin/python'
 
 -- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+-- They will automatically be installed.
 --
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
@@ -668,26 +624,6 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    --  I found these autocompletes to be too aggressive
-    --  If I don't reinstate this after a while, delete it
-    --   ['<Tab>'] = cmp.mapping(function(fallback)
-    --     if cmp.visible() then
-    --       cmp.select_next_item()
-    --     elseif luasnip.expand_or_locally_jumpable() then
-    --       luasnip.expand_or_jump()
-    --     else
-    --       fallback()
-    --     end
-    --   end, { 'i', 's' }),
-    --   ['<S-Tab>'] = cmp.mapping(function(fallback)
-    --     if cmp.visible() then
-    --       cmp.select_prev_item()
-    --     elseif luasnip.locally_jumpable(-1) then
-    --       luasnip.jump(-1)
-    --     else
-    --       fallback()
-    --     end
-    --   end, { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lsp' },

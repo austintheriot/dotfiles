d3bc8f467f774615bab67bf563238bed9bf442e7
Austin Theriot <austinmtheriot@gmail.com>
2023-09-21 21:23:14 -0500

    merge in changes from work

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -236,26 +236,63 @@ require('lazy').setup({
     end,
   },
 
-  -- enable using a dedicated file view (netrw doesn't work great on MacOS)
+  -- icons used for numerous plugins
+  'nvim-tree/nvim-web-devicons',
+
+  -- file explorer
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
+    "nvim-tree/nvim-tree.lua",
     config = function()
-      require("neo-tree").setup({
-        window = {
-          mappings = {
-            ["-"] = "navigate_up"
-          }
+      -- disable netrw at the very start of your init.lua
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      -- set termguicolors to enable highlight groups
+      vim.opt.termguicolors = true
+
+      -- empty setup using defaults
+      local HEIGHT_RATIO = 0.8 -- You can change this
+      local WIDTH_RATIO = 0.5  -- You can change this too
+
+      require('nvim-tree').setup({
+        disable_netrw = true,
+        hijack_netrw = true,
+        respect_buf_cwd = true,
+        sync_root_with_cwd = true,
+        view = {
+          relativenumber = true,
+          float = {
+            enable = true,
+            open_win_config = function()
+              local screen_w = vim.opt.columns:get()
+              local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+              local window_w = screen_w * WIDTH_RATIO
+              local window_h = screen_h * HEIGHT_RATIO
+              local window_w_int = math.floor(window_w)
+              local window_h_int = math.floor(window_h)
+              local center_x = (screen_w - window_w) / 2
+              local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                  - vim.opt.cmdheight:get()
+              return {
+                border = "rounded",
+                relative = "editor",
+                row = center_y,
+                col = center_x,
+                width = window_w_int,
+                height = window_h_int,
+              }
+            end,
+          },
+          width = function()
+            return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+          end,
         },
       })
-      vim.keymap.set('n', '<leader>-', '<Cmd>Neotree toggle<CR>')
+      vim.keymap.set("n", "<leader>-", ':NvimTreeToggle<CR>')
     end
   },
+
+
 
   {
     -- Highlight, edit, and navigate code
@@ -659,7 +696,6 @@ cmp.setup {
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
     },
   },
   sources = {

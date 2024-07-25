7be74c23b439c9c901e414f845dd568f64b05a44
Austin Theriot <austin@gingerlabs.com>
2024-07-25 16:11:44 -0500

    more refactors, clean up .zshrc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,19 +25,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-    lazy = false,
-    config = function()
-      local null_ls = require('null-ls')
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.prettierd,
-        },
-      })
-    end
-  },
-
   require 'plugins.icons',
   require 'plugins.telescope',
   require 'plugins.treesitter',
--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -13,7 +13,7 @@ vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+yg')
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>yy", '"+yy')
--- past from clipboard
+-- paste from clipboard
 vim.keymap.set("n", "<leader>p", '"+p')
 vim.keymap.set("n", "<leader>P", '"+P')
 vim.keymap.set("v", "<leader>p", '"+p')

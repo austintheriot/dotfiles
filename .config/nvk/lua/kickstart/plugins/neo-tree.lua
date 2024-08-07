254b34103a0e2629d2479420c2dd948771c7ba59
Austin Theriot <austin@gingerlabs.com>
2024-08-07 11:38:57 -0500

    enable neo-tree

--- .config/nvk/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvk/lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+    { '<leader>o', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<leader>o'] = 'close_window',
         },
       },
     },

a189067092cf305cd68ad2291cfaa8c5d62a418e
Austin Theriot <austinmtheriot@gmail.com>
2024-08-08 21:40:02 -0500

    add scripts for terms AND code

--- .my-scripts/tmux-split.sh
+++ .my-scripts/tmux-split.sh
@@ -1,4 +1,23 @@
-# splits pane into left code editor and two side terminals
-tmux split-window -h
-tmux split-window -v 
-tmux select-pane -L
+# #!/bin/zsh
+
+SPLIT_TYPE=$1
+
+# split into 4 vertically stacked terminals, select the top-most one
+if [[ $SPLIT_TYPE = "terms" ]]; then
+  tmux split-window -v
+  tmux split-window -v
+  tmux select-pane -D
+  tmux split-window -v
+  tmux select-pane -U
+fi 
+
+ # splits pane into left code editor and two side terminals, select the leftmost one
+if [[ $SPLIT_TYPE = "code" ]]; then
+  tmux split-window -h
+  tmux split-window -v 
+  tmux select-pane -L
+fi 
+
+
+
+
--- .my-scripts/tmux-start.sh
+++ .my-scripts/tmux-start.sh
@@ -1,9 +1,22 @@
-# check for session already open
-SESSION_NAME='code'
+# #!/bin/zsh
+
+# Return early if already inside a tmux session
+# We don't want to nest tmux sessions!
+if [ -n "$TMUX" ]; then
+    echo "A tmux session is already open."
+    return
+fi
+
+# check if session already exists
+# if it doesn't exist yet, create it
+SESSION_NAME=$1
 if [[ "$(tmux ls | rg $SESSION_NAME)" = "" ]]; then
-  # open new session
+  # open new session 
   tmux new-session -d -s $SESSION_NAME
+  # split windows--use same argument as the name of the session
   source ~/.my-scripts/tmux-split.sh
 fi 
+
+# attach the session
 tmux attach -t $SESSION_NAME
 

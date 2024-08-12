6bd902de415c9144cc37abaf9d8fc0d7ffd448ef
Austin Theriot <austin@gingerlabs.com>
2024-08-12 11:13:31 -0500

    allow specifying split numbers for code

--- .my-scripts/tmux-split.sh
+++ .my-scripts/tmux-split.sh
@@ -1,22 +1,38 @@
-# #!/bin/sh
+#!/bin/sh
 
 # split the attached tmux session into pre-configured panes
 
+# Function to split tmux panes vertical-terminal-style
+split_tmux_terminal_panes() {
+  if [[ $1 = "2" ]]; then
+    tmux split-window -v
+    tmux select-pane -U
+  elif [[ $1 = "4" ]]; then
+    tmux split-window -v
+    tmux split-window -v
+    tmux select-pane -D
+    tmux split-window -v
+    tmux select-pane -U
+  else
+    echo "Invalid number of splits: $NUM_SPLITS"
+  fi
+}
+
+
 SPLIT_TYPE=$1
+
+# Fallback to 4
+NUM_SPLITS=${2:-4}
 
 # split into 4 vertically stacked terminals, select the top-most one
 if [[ $SPLIT_TYPE = "terms" ]]; then
-  tmux split-window -v
-  tmux split-window -v
-  tmux select-pane -D
-  tmux split-window -v
-  tmux select-pane -U
-fi 
+  split_tmux_terminal_panes "$NUM_SPLITS"
+fi
 
  # splits pane into left code editor and two side terminals, select the leftmost one
 if [[ $SPLIT_TYPE = "code" ]]; then
   tmux split-window -h
-  tmux split-window -v 
+  split_tmux_terminal_panes "$NUM_SPLITS"
   tmux select-pane -L
 fi 
 

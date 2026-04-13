62f1f0c7bf05c962183fd3f9fe76b052aee5c0f9
Austin Theriot <austin@gingerlabs.com>
2026-04-13 12:17:07 -0500

    create more work directories

--- .my-scripts/tmux-setup.sh
+++ .my-scripts/tmux-setup.sh
@@ -41,14 +41,20 @@ setup_code_layout 3
 tmux new-window -t $SESSION_NAME -n "4" -c ~/Documents/Code/Notability-My-Work-4
 setup_code_layout 4
 
-tmux new-window -t $SESSION_NAME -n "Staging" -c ~/Documents/Code/Notability-Staging
+tmux new-window -t $SESSION_NAME -n "4" -c ~/Documents/Code/Notability-My-Work-5
 setup_code_layout 5
 
-tmux new-window -t $SESSION_NAME -n "Reviews" -c ~/Documents/Code/Notability-Reviews
+tmux new-window -t $SESSION_NAME -n "4" -c ~/Documents/Code/Notability-My-Work-6
 setup_code_layout 6
 
-tmux new-window -t $SESSION_NAME -n "Config" -c ~
+tmux new-window -t $SESSION_NAME -n "Staging" -c ~/Documents/Code/Notability-Staging
 setup_code_layout 7
+
+tmux new-window -t $SESSION_NAME -n "Reviews" -c ~/Documents/Code/Notability-Reviews
+setup_code_layout 8
+
+tmux new-window -t $SESSION_NAME -n "Config" -c ~
+setup_code_layout 9
 
 # Update window names with git branches
 ~/.my-scripts/tmux-update-window-names.sh $SESSION_NAME
